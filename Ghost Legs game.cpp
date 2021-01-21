702b2ddbd46c51f6f48d2dc0a3e2ac315e9d82a0
Artem Popov <70245014+strBCL1@users.noreply.github.com>
2021-01-21 10:48:23 +0100

    Update Ghost Legs game.cpp

--- Ghost Legs game.cpp
+++ Ghost Legs game.cpp
@@ -1,41 +1,36 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> //Library to store primary needed functions
 using namespace std;
 #define ll long long
 #define mp make_pair
 #define pb push_back
-int goRight(vector<string> v, int curRow, int curPos); //Function to go right to next column
-int goLeft(vector<string> v, int curRow, int curPos); //Function to go left to next column
+int goRight(vector<string> v, int curRow, int curPos); //Function prototype to go right to next column
+int goLeft(vector<string> v, int curRow, int curPos); //Function prototype to go left to next column
 
 //Function to get pair of top char <-> down char
 char getPair(char c, vector<string> v, int w, int h) {
     int i = 1, curPos = v.front().find(c);
 
-    //cout << "\n" << c << ":\n";
-
     while (i < h) {
-        //If it's a down char, return this character
+        //If it's a very down char, return this character
         if (i == h-1) { return v[i][curPos]; }
 
         //Else
         else {
-            //cout << i << "\n";
-
-            //if it's a pipe ('|')
+            //If current char is a pipe ('|')
             if (v[i][curPos] == '|') {
 
-                //If there is a '-' to its right, go right
+                //If there is a '-' to the right of current position, go right
                 if (curPos >= 0 && v[i][curPos+1] == '-') { 
-                    //cout << i << " " << curPos << "\n"; 
                     curPos = goRight(v, i, curPos);
                 }
 
-                //Else if there is a '-' to its right, go right
+                //Else if there is a '-' to the left of current position, go left
                 else if (curPos > 0 && curPos <= w && v[i][curPos-1] == '-') { 
-                    //cout << i << " " << curPos << "\n";
                     curPos = goLeft(v, i, curPos);
                 }
             }
-            //cout << i << " " << curPos << "\n"; 
+
+            //Move down
             ++i;
         }
     }
@@ -45,19 +40,19 @@ char getPair(char c, vector<string> v, int w, int h) {
 
 //Function to get pairs (top char <-> down char)
 void playGame(vector<string> v, int w, int h) {
-    //Declare a map, which will store the pairs of top char <-> down char
+    //Declare an unordered map, which will store the pairs of top char <-> down char
     unordered_map<char, char> m;
 
     //Declare vectors, which will store top chars chars to start the game
     vector<char> top;
 
-    //Get top chars
+    //Get top chars, which are not a ' ' char
     for (auto it : v.front()) if (!isspace(it)) top.pb(it);
 
     //For each char in top chars, get its pair from down chars
-    for (int i = 0; i < top.size(); ++i) {
+    for (int i = 0; i < top.size(); ++i) 
         m[top[i]] = getPair(top[i], v, w, h);
-    }
+    
 
     //Print the answer
     for (auto it : top)
@@ -72,17 +67,17 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     
-    //Declare width and height; input width and height
-    int w, h; cin >> w >> h; cin.ignore(); 
+    //Declare width and height; input width and height; clean the input buffer (stream)
+    int w, h; cin >> w >> h; cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
 
-    //Create a vector of strings which will store the Ghost Legs
+    //Create a vector of strings of size 'h', which will store the Ghost Legs strings
     vector<string> v(h);
 
-    //Input Ghost Legs
+    //Input Ghost Legs strings
     for (int i = 0; i < h; ++i)
         getline(cin, v[i]);
 
-    //Call a function to get pairs (top char <-> down char)
+    //Call a function to get pairs to each top character
     playGame(v, w, h);
 
     return 0;
